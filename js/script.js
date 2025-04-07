713bcb7cd83720316451c9840be04b01c2a88584
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-04-07 14:59:48 +0000

    Area of Trap

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 BrandonBCode All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: BrandonBCode
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates the salary.
+ */
+function calculateArea() {
+  // input that takes information from the user and converts it.
+  let sideA = parseFloat(document.getElementById('side-a').value);
+  let sideB = parseFloat(document.getElementById('side-b').value);
+  let sideH = parseFloat(document.getElementById('side-height').value);
+  
+
+  // process of the calculation.
+    let areaofTrapezoid = sideA + sideB / 2 * sideH;
+
+  // output that displays the information.
+    document.getElementById("area-trapezoid").innerHTML = "The area of the trapezoid is: " + areaofTrapezoid.toFixed(1) + " cm²";
+
+}
